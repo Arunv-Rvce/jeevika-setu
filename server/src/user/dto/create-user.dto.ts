56135c741eb2e8bdc5791d48335610d97cfc9e99
Arun <arunvrvce@gmail.com>
2020-05-03 10:13:51 +0530

    fixed the username validation message

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -4,8 +4,8 @@ export class CreateUserDto {
     @IsEmail({}, {message: "invalid email"})
     readonly email: string;
 
-    @MaxLength(15, {message: "user name can't be more than 15 character"})
-    @MinLength(3, {message: "user name can't be less than 3 character"})
+    @MaxLength(15, {message: "user name can't be more than 15 characters"})
+    @MinLength(3, {message: "user name can't be less than 3 characters"})
     @IsString()
     readonly username: string;
 }
